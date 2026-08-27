90826a80143165cde160d7c06adeffe715494a9a
Jim Judd <jajudd+paypal@paypal.com>
2026-08-27 11:25:27 -0500

    feat: add team color and abbreviation lookup tables

--- scorebook.py
+++ scorebook.py
@@ -46,6 +46,17 @@ POS = {
     "10": ("D", "DH"), "11": ("-", "PH"), "12": ("-", "PR"),
 }
 
+# MLB team abbreviation -> team ID, for --team CHC on the CLI. The HTML
+# picker doesn't need this -- it lists every game straight from the
+# schedule endpoint without filtering by team.
+TEAM_IDS = {
+    "LAA": 108, "ARI": 109, "BAL": 110, "BOS": 111, "CHC": 112, "CIN": 113,
+    "CLE": 114, "COL": 115, "DET": 116, "HOU": 117, "KC": 118, "LAD": 119,
+    "WSH": 120, "NYM": 121, "OAK": 133, "PIT": 134, "SD": 135, "SEA": 136,
+    "SF": 137, "STL": 138, "TB": 139, "TEX": 140, "TOR": 141, "MIN": 142,
+    "PHI": 143, "ATL": 144, "CWS": 145, "MIA": 146, "NYY": 147, "MIL": 158,
+}
+
 
 # --------------------------------------------------------------------------
 # fetch
